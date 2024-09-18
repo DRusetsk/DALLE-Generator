7af9b18d8fc43622cef44f8a7ca197088176c734
Daniel <dukarus007@gmail.com>
2024-09-17 20:40:17 -0400

    components added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {Loader,Card,FormField} from '../components'
 
 const Home = () => {
+  const[loading,setLoading]=useState(false)
+  const [allPosts,setAllPosts]=useState(null)
+
   return (
-    <div>Home</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>The Community Showcase</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Browse through a selection of previously generated images all created by DALL-E AI</p>
+      </div>
+      <div className='mt-16'>
+        sd
+      </div>
+    </section>
   )
 }
 
