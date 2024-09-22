19281b62436ba629d93c94bf9e738c9197935f47
Daniel <dukarus007@gmail.com>
2024-09-21 23:23:57 -0400

    Cloudinary Configured

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { response } from 'express'
 import * as dotenv from 'dotenv'
 import {v2 as cloudinary} from 'cloudinary'
 
@@ -7,3 +7,11 @@ import Post from '../mongodb/models/post.js'
 dotenv.config()
 
 const router=express.Router()
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    cloud_name:process.env.CLOUDINARY_API_SECRET
+})
+
+export default router
