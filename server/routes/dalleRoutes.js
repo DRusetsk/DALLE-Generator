7453e4bc9197b872a6f77c133d147418fb5382b5
Daniel <dukarus007@gmail.com>
2024-09-21 23:15:28 -0400

    Image generation back/front linked

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,7 +1,39 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
-import {Configuration,OpenAIApi} from 'openai'
+import OpenAI from 'openai'
 
 dotenv.config()
 
 const router=express.Router()
+
+const openai=new OpenAI({
+    apiKey:process.env.OPENAI_API_KEY
+})
+
+router.route('/').get((req,res)=>{
+    res.send('Hello from DALL-E!')
+})
+
+router.route('/').post(async(req,res)=>{
+    try {
+        const{prompt}=req.body
+        /*
+        const aiResponse=await OpenAI.createImage({
+            prompt,
+            n:1,
+            size:'1024x1014',
+            response_format:'b64_json',
+        })
+        const image=aiResponse.data.data[0].image.b64_json
+        res.status(200).json({photo:image})
+        */
+        const aiResponse = await openai.images.generate({prompt:prompt,n:1, size:'1024x1024', response_format:'b64_json'})
+        const image = aiResponse.data[0].b64_json
+        return res.status(200).json({photo: image})
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error?.response.data.error.message)
+    }
+})
+
+export default router;
