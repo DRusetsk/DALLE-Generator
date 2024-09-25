ecf860aac63c0b0b444d03db5a1b608df59f0deb
Daniel <dukarus007@gmail.com>
2024-09-25 14:08:25 -0400

    Added download image function

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,6 @@
+import FileSaver from 'file-saver';
 import { surpriseMePrompts } from '../constants';
+import FileSave from 'file-saver'
 
 export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
@@ -7,4 +9,8 @@ export function getRandomPrompt(prompt) {
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
   return randomPrompt;
+}
+
+export async function downloadImage(_id,photo){
+  FileSaver.saveAs(photo,'download-${_id}.jpeg')
 }
