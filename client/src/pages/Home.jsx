930519d1a6dbd1ef01bd35912b26e5c014cce7db
Daniel <dukarus007@gmail.com>
2024-09-25 14:52:34 -0400

    Completed Showcase

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,21 @@
 import React,{useState,useEffect} from 'react'
 import {Loader,Card,FormField} from '../components'
 
-const RenderCards = ({ data, title }) => {
-  if (data?.length > 0) {
-    return (
-      data.map((post) => <Card key={post._id} {...post} />)
-    );
-  }
-
+const RenderCards=({data,title})=>{
+  if(data?.length>0){return data.map((post)=> <Card key={post._id}{...post}/>)}
   return (
-    <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
-  );
-};
+    <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
+  )
+}
 
 const Home = () => {
   const[loading,setLoading]=useState(false)
   const [allPosts,setAllPosts]=useState(null)
   const[searchText,setSearchText]=useState('')
+  const [searchedResults, setSearchedResults] = useState(null)
+  const [searchTimeout, setSearchTimeout] = useState(null)
 
-  useEffect(()=>{
+  useEffect(()=>{{/*Fetch posts function*/}
     const fetchPosts=async()=>{
       setLoading(true)
       try {
@@ -39,6 +36,19 @@ const Home = () => {
     fetchPosts()
   },[])
 
+const handleSearchChange=(e)=>{
+  clearTimeout(searchTimeout)
+  setSearchText(e.target.value)
+
+  setSearchTimeout(
+    setTimeout(()=>{
+      const searchResults=allPosts.filter((item)=>item.name.toLowerCase().includes(searchText.toLowerCase())||item.prompt.toLowerCase().includes(searchText.toLowerCase()))
+      setSearchedResults(searchResults)
+    },500)
+  )
+
+}
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -46,7 +56,8 @@ const Home = () => {
         <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Browse through a selection of previously generated images all created by DALL-E AI</p>
       </div>
       <div className='mt-16'>
-        <FormField/>
+        <FormField
+        labelName='SearchPosts' type='text' name='text' placeholder='Search Posts' value={searchText} handleChange={handleSearchChange}/>
       </div>
 
       <div className='mt-10'>
@@ -64,7 +75,7 @@ const Home = () => {
             <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
               {searchText?(
                 <RenderCards
-                  data={[]}
+                  data={searchedResults}
                   title="No search results found"
                 />
               ):(
